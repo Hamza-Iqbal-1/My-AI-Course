e9d641f8e79696f1ecf738950bc6ee27ea56f077
Hamza-Iqbal-1 <hamzaiqbalofficial9@gmail.com>
2025-07-11 19:25:42 +0500

    specific data type

--- Case5-SpecificDataType.py
+++ Case5-SpecificDataType.py
@@ -2,44 +2,43 @@
 strVar = str("Hello World - Richard")
 print(type(strVar))
 
-strVar2="Hello World - Richard"
-print(type(strVar2))
+# strVar2="Hello World - Richard"
+# print(type(strVar2))
 
 
-#---------------------- int
+# #---------------------- int
 
-intVar = int(50)
-print(type(intVar))
+# intVar = int(50)
+# print(type(intVar))
 
-intVar2=50
-print(type(intVar2))
-
-
-#---------------------- float
-
-floatVar = float(50.3)
-print(type(floatVar))
-
-floatVar2=50.3
-print(type(floatVar2))
-
-#---------------------
+# intVar2=50
+# print(type(intVar2))
 
 
-#---------------------- float = with an "e" to indicate the power of 10
+# #---------------------- float
 
-floatEVar = float(35e3)
-print(type(floatEVar))
+# floatVar = float(50.3)
+# print(type(floatVar))
 
-floatEVar2=35e3
-print(type(floatEVar2))
+# floatVar2=50.3
+# print(type(floatVar2))
 
-#---------------------
+# #---------------------
 
-#---------------------- Complex = numbers are written with a "j" as the imaginary part
-ComplexVar = complex(3+5j)
-print(type(ComplexVar))
 
-ComplexVar2=3+5j
-print(type(ComplexVar2))
+# #---------------------- float = with an "e" to indicate the power of 10
 
+# floatEVar = float(35e3)
+# print(type(floatEVar))
+
+# floatEVar2=35e3
+# print(type(floatEVar2))
+
+# #---------------------
+
+# #---------------------- Complex = numbers are written with a "j" as the imaginary part
+# ComplexVar = complex(3+5j)
+# print(type(ComplexVar))
+
+# ComplexVar2=3+5j
+# print(type(ComplexVar2))
